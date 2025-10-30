6a785d6734b94ef907604faed8d725c143acd450
Silviu Stana <gamedevseals@gmail.com>
2025-10-30 10:38:17 +0200

    Weather App assignment
    -routes
    -strongly typed views
    -controllers passing either a collection of cities, or 1 single city
    -page navigation

---
+++ ViewsExample/ViewsExample/Controllers/WeatherController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViewsExample.Models;
+
+namespace ViewsExample.Controllers
+{
+    public class WeatherController : Controller
+    {
+        [Route("/weather")]
+        public IActionResult Weather()
+        {
+            List<CityWeather> cityWeathers =
+            [
+               new() { CityUniqueCode = "LDN", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 8:00"),  TemperatureFahrenheit = 33 },
+               new() { CityUniqueCode = "NYC", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 3:00"),  TemperatureFahrenheit = 60 },
+               new() { CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = Convert.ToDateTime("2030-01-01 9:00"),  TemperatureFahrenheit = 82 },
+            ];
+
+
+            return View(cityWeathers);
+        }
+
+
+        [Route("/weather/{cityCode}")]
+        public IActionResult CityWeather(string? cityCode)
+        {
+            List<CityWeather> allCities =
+            [
+               new() { CityUniqueCode = "LDN", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 8:00"),  TemperatureFahrenheit = 33 },
+               new() { CityUniqueCode = "NYC", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 3:00"),  TemperatureFahrenheit = 60 },
+               new() { CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = Convert.ToDateTime("2030-01-01 9:00"),  TemperatureFahrenheit = 82 },
+            ];
+
+            CityWeather? cityWeather = allCities.Where(c => c.CityUniqueCode == cityCode).FirstOrDefault();
+
+            if (cityWeather == null) return Content("City not found!");
+
+            return View(cityWeather);
+    }
+}
+}
---
+++ ViewsExample/ViewsExample/Models/CityWeather.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViewsExample.Models
+{
+    public class CityWeather
+    {
+        [Required]
+        public string? CityUniqueCode;
+        public string? CityName;
+        public DateTime DateAndTime;
+        public int TemperatureFahrenheit;
+    }
+}
