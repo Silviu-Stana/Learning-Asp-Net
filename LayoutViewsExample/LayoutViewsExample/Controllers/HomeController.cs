83486354da96ee00983f75f53e7280f7d742b353
Silviu Stana <gamedevseals@gmail.com>
2025-10-31 20:31:09 +0200

    Learning partial and layout views

---
+++ LayoutViewsExample/LayoutViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using LayoutViewsExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace LayoutViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("/contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [Route("/programming-languages")]
+        public IActionResult ProgrammingLanguages()
+        {
+            ListModel listModel = new() { Items = new List<string>() { "Python", "C#", "Go"},Title= "ProgrammingLanguages List" };
+            return PartialView("_ListPartialView", listModel);
+        }
+    }
+}
---
+++ LayoutViewsExample/LayoutViewsExample/Models/ListModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LayoutViewsExample.Models
+{
+    public class ListModel
+    {
+        public string Title { get; set; }
+        public List<string> Items { get; set; } = [];
+    }
+}
