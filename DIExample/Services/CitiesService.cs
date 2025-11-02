40af8a2fd1473467c2085b82f89804e8a5d4d4d3
Silviu Stana <gamedevseals@gmail.com>
2025-11-02 13:11:08 +0200

    Learning dependency injection

---
+++ DIExample/Services/CitiesService.cs
@@ -0,0 +1,18 @@
+﻿using ServiceContracts;
+
+namespace Services
+{
+    public class CitiesService : ICitiesService
+    {
+        private List<string> _cities = new List<string>();
+
+        public CitiesService() {
+            _cities = new List<string>
+            {
+                "London", "Paris", "New York", "Tokyo", "Rome"
+            };
+        }
+
+        public List<string> GetCities() => _cities;
+    }
+}
