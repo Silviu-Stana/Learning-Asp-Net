3a3598f89295439de33392a5db52889b71a617bc
Silviu Stana <gamedevseals@gmail.com>
2025-11-17 08:53:31 +0200

    Fix: make country show up in Grid

--- CRUDExample/Services/PersonService.cs
+++ CRUDExample/Services/PersonService.cs
@@ -60,7 +60,7 @@ namespace Services
 
         public List<PersonResponse> GetAllPersons()
         {
-            return _persons.Select(person => person.ToPersonResponse()).ToList();
+            return _persons.Select(person => ConvertPersonToPersonResponse(person)).ToList();
         }
 
         public PersonResponse? GetPersonById(Guid? personId)
@@ -71,7 +71,7 @@ namespace Services
             
             if (person == null) return null;
 
-            return person.ToPersonResponse();
+            return ConvertPersonToPersonResponse(person);
         }
 
         #region SortingFunctions
@@ -185,7 +185,7 @@ namespace Services
             match.CountryID = request.CountryID;
             match.ReceiveNewsLetters = request.ReceiveNewsLetters;
 
-            return match.ToPersonResponse();
+            return ConvertPersonToPersonResponse(match);
         }
 
         public bool DeletePerson(Guid? personId)
